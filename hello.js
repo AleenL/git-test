3fea379753b308d140275490943b79e5eb12afc1
784757555@qq.com <784757555@qq.com>
2017-03-31 21:01:21 +0800

    I don't kown

---
+++ hello.js
@@ -0,0 +1,11 @@
+"use strict"
+console.log("hello world")
+var s="hello world";
+for (i = 0;i<s.length;i++){
+	i++
+	var a=s.substring(0, i)
+	return a 
+}
+setTimeout(function(){
+	console.log(a)
+},1000)
